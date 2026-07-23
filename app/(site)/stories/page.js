02bf9a7a06ac72182bbddd110e062cbccb197ff0
luteishee <luteishee@yandex.ru>
2026-07-23 16:39:26 +0300

    Update page.js

--- app/(site)/stories/page.js
+++ app/(site)/stories/page.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import { supabase } from '../../../lib/supabaseClient';
-import VideoFrame from '../../components/VideoFrame';
+import VideoFrame from '../../../app/components/VideoFrame';
 
 export const dynamic = 'force-dynamic';
 
